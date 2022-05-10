7c0236950687b84d3318a133fb854a560a001295
DonSlon1 <lukindihel@gmail.com>
2022-05-10 17:19:36 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("article").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("article").style.marginLeft = "100px";
+    }
