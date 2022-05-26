3e3045e261393d312f90bf1393feaa8d25c63df9
DonSlon1 <lukindihel@gmail.com>
2022-05-26 23:01:32 +0200

    1

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ function openNav() {
     document.getElementById("article").style.marginLeft = "250px";
     }
 
-    function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0px";
     document.getElementById("article").style.marginLeft = "100px";
     }
+function listClose(){
+    document.getElementById("misninavigace").style.display = "none";
+    document.getElementById("sipka").outerHTML='<span style="display: inline; margin-left: 30px;" id="sipka" onclick="listOpne()">▼</span>';
+}
+function listOpne(){
+    document.getElementById("misninavigace").style.display = "block";
+    document.getElementById("sipka").outerHTML='<span style="display: inline; margin-left: 30px;" id="sipka" onclick="listClose()">▲</span>';
+}
